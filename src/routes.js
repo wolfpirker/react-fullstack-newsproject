0e3e8fb710ca3af1c6e8a664aba0f74c6de9fdcc
wolfpirker <w_pirker@gmx.de>
2021-12-20 19:10:37 +0100

    include a Header and main layout

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import MainLayout from './hoc/mainLayout';
 
 import Home from './components/home';
+import Header from './components/header';
 
 const Routes = () => (
     <BrowserRouter>
-        <Switch>
-            <Route path="/" component={Home} />
-        </Switch>
+        <Header />
+        <MainLayout>
+            <Switch>
+                <Route path="/" component={Home} />
+            </Switch>
+        </MainLayout>
     </BrowserRouter>
 )
 
