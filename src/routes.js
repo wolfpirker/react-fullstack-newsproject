94e3280ad9eb56b4bd3de416b9d262d0c30bbb6f
wolfpirker <w_pirker@gmx.de>
2021-12-20 18:54:58 +0100

    setting up router, and redux

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
+import Home from './components/home';
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={Home} />
+        </Switch>
+    </BrowserRouter>
+)
+
+
+export default Routes
